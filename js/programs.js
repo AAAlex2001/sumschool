cbd5e55f15626f023d098650ec377492b3c1db10
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-04-27 15:40:49 +0300

    1

--- js/programs.js
+++ js/programs.js
@@ -5,24 +5,20 @@ async function initPage() {
     await loadComponent('#marquee-root', 'marquee.html');
     await mountFooter();
 
-    // Теперь элемент .container-marquee гарантированно существует в DOM
     initMarquee();
 }
 
 function initMarquee() {
-    const el = document.querySelector('.container-marquee'); // исправленный селектор
-    if (el && typeof Marquee !== 'undefined') {
-        new Marquee(el, {
-            duration: 20000,
-            gap: 50,
-            delayBeforeStart: 0,
-            direction: 'left',
-            duplicated: true
+    const el = document.querySelector('.container-marquee');
+
+    if (el && typeof marqueeJs === 'function') {
+        marqueeJs(el, {
+            speed: 20,
+            delay: 0
         });
     } else {
         console.warn('Marquee не инициализирован: элемент или библиотека не найдены');
     }
 }
 
-// Запускаем инициализацию страницы
 initPage();
